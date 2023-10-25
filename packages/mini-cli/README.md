60ce8aa5188f5de33fd213ee5722cfc901098087
yq <yq@192.168.0.103>
2023-10-26 00:20:38 +0800

    应该使用simple-git下载仓库

--- packages/mini-cli/README.md
+++ packages/mini-cli/README.md
@@ -2,6 +2,17 @@
 
 > TODO: description
 
+## tips
+
+- simple-git才是git下载代码的仓库
+  - simple-get是封装的http
+  - vue cli里使用的好像不是 simple-git，后续再研究下他们的
+  - 目前下载仓库时，没有loading，不友好，也没有一些其他提示，TODO
+
+- [x] 通过 `.npmrc` 配置仓库的rigistry源
+- [x] simple-git 已经正常下载
+- [ ] 添加下载 loading 以及其他提示
+- [ ] 观察其他cli库里如何使用 git下载工具的
 ## Usage
 
 ```
--- packages/mini-cli/lib/vue-cli.js
+++ packages/mini-cli/lib/vue-cli.js
@@ -1,5 +1,6 @@
 import inquirer from 'inquirer'
-import simpleGet from 'simple-get'
+// import simpleGet from 'simple-get'
+import { simpleGit } from 'simple-git';
 
 export default function handleVueTemplate() {
     const vueQuestions = [
@@ -25,24 +26,40 @@ export default function handleVueTemplate() {
 
     inquirer
         .prompt(vueQuestions)
-        .then((answsers) => {
-            console.log('djch answsers', answsers)
+        .then(async (answsers) => {
+            // console.log('djch answsers', answsers)
             const remote = 'https://github.com/bencodezen/vue-enterprise-boilerplate.git';
             // console.log('djch ', Object.keys(simpleGet), simpleGet.get.toString())
             // https://github.com/bencodezen/vue-enterprise-boilerplate.git
-            simpleGet.get(remote, function (err, res) {
-                if (err) {
-                    console.error(err)
-                    throw err
-                }
-                // console.log('ddd', res) // 200
-                // res.pipe(process.stdout) // `res` is a stream
-              })
-              
-
-            // simpleGet('./', () => {
-            //     console.log('ddd')
+            // simpleGet.get(remote, function (err, res) {
+            //     if (err) {
+            //         console.error(err)
+            //         throw err
+            //     }
+            //     // console.log('ddd', res) // 200
+            //     // res.pipe(process.stdout) // `res` is a stream
             // })
+
+
+            const options = {
+                baseDir: process.cwd(),
+                binary: 'git',
+                maxConcurrentProcesses: 6,
+                trimmed: false,
+             };
+             
+             // when setting all options in a single object
+             const git = simpleGit(options);
+             try {
+                console.log(1, process.cwd())
+                // process.cwd() bin目录同级，git clone 的参数2也是相对该目录
+                await git.clone(remote, './test1/');
+                console.log(2)
+             } catch (e) {
+                /* handle all errors here */
+                console.log('djch e', e)
+             }
+
         })
         .catch((err) => console.log('djch err', err))
 }
